04d3fb7739238b467702235251af53ec998cad11
vashneyakash <vashneyakash@gmail.com>
2022-02-03 11:24:10 +0530

    [leetcode-google] Practice K-empty slots question

--- leetcode/google/java/kEmptySlots.java
+++ leetcode/google/java/kEmptySlots.java
@@ -1,13 +1,32 @@
 package google.java;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 
 class kEmptySlots {
 
     public static void main(String[] args) {
-        AtomicInteger atomicInteger = new AtomicInteger(1);
-        atomicInteger.compareAndSet(2, 5);
-        System.out.println("Vlaue of atomicInteger : " + atomicInteger.get());
-        atomicInteger.incrementAndGet();
+        int n = 3;
+        List<Integer> bloomingFlowerCoordinate = Arrays.asList(1, 3, 2);
+        int k = 1;
+
+        List<Integer> flowersBloomingDay = new ArrayList<>(n);
+        for (int i = 0; i < n ; i++) {
+            flowersBloomingDay.add(null);
+        }
+        for (int i = 0; i < bloomingFlowerCoordinate.size(); i++) {
+            flowersBloomingDay.set(bloomingFlowerCoordinate.get(i), i);
+        }
+
+        int left = 0, right = k + 1, ans = 100000;
+        for (int i = 0; right < n; i++) {
+           if (i == right) {
+               ans = Math.min(ans, Math.max(flowersBloomingDay.get(left), flowersBloomingDay.get(right)));
+           } else if (flowersBloomingDay.get(i) < flowersBloomingDay.get(left) || flowersBloomingDay.get(i) < flowersBloomingDay.get(right)) {
+               left = i;
+               right = i + k + 1;
+           }
+        }
+
+        System.out.println( (ans == 100000) ? -1 : ans);
     }
 }
