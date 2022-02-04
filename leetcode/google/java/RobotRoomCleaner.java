7b0c975f542688688b25e74441765339df8834af
vashneyakash <vashneyakash@gmail.com>
2022-02-04 11:35:08 +0530

    [bruteforce-solution] Think for brute-force for RobotRoomCleaner

---
+++ leetcode/google/java/RobotRoomCleaner.java
@@ -0,0 +1,9 @@
+package google.java;
+
+public class RobotRoomCleaner {
+    /*
+    * Brute force:
+    * Start with current cell, do dfs, maintain visited set
+    * try all possible directions. If no move left backtrack
+    * */
+}
