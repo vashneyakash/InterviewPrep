2f5b220d7b78cb8c418f54963b60cd0456ebfa35
vashneyakash <vashneyakash@gmail.com>
2022-02-06 11:40:26 +0530

    [brute-force-solution] Start divide chocolate code

---
+++ leetcode/google/java/divideChocolate.java
@@ -0,0 +1,10 @@
+package google.java;
+
+public class divideChocolate {
+    /*
+    * Brute Force:
+    * there are n elements we can separate them in n C k ways
+    * generate all possible partitions and find solution
+    *
+    * */
+}
