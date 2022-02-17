e77aee9e0f8ecb1963d7cde3e42d6a1dfaf2151b
vashneyakash <vashneyakash@gmail.com>
2022-02-17 11:45:08 +0530

    [brute-force-solution] Start thinking on SplitArrayMaximumSum

---
+++ leetcode/google/java/SplitArrayMaximumSum.java
@@ -0,0 +1,13 @@
+package google.java;
+
+public class SplitArrayMaximumSum {
+    /*
+    * Brute force:
+    * Try all combinations which is (n-1) C m
+    *
+    * Optimal solution:
+    * Try binary search.
+    * Hi = Total sum of array
+    * Lo = The Lowest element in array
+    * */
+}
