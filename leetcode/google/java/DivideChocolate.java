0f1dae2ed44924d5e8040db909b04d3025365cb0
vashneyakash <vashneyakash@gmail.com>
2022-02-19 11:56:24 +0530

    [optimal-solution] Finish DivideChocolate

--- leetcode/google/java/DivideChocolate.java
+++ leetcode/google/java/DivideChocolate.java
@@ -1,5 +1,6 @@
 package google.java;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -26,16 +27,50 @@ public class DivideChocolate {
 
     public int maximumMinimumSweetnessSegmentAfterCutting() {
         int totalSweetnessOfChocolateBar = sweetnessBar.stream().flatMapToInt(IntStream::of).sum();
-        int maximumSweetnessThreshold = totalSweetnessOfChocolateBar/(numberOfCuts + 1);
+//        int maximumSweetnessThreshold = totalSweetnessOfChocolateBar/(numberOfCuts + 1);
+        int maximumSweetnessThreshold = totalSweetnessOfChocolateBar;
         int minimumSweetnessThreshold = sweetnessBar.stream().flatMapToInt(IntStream::of).min().getAsInt();
-
+        return maximumMinimumSweetnessSegmentAfterCuttingUsingBinarySearch(maximumSweetnessThreshold, minimumSweetnessThreshold);
     }
 
     public int maximumMinimumSweetnessSegmentAfterCuttingUsingBinarySearch(int maximumSweetnessThreshold, int minimumSweetnessThreshold) {
-        int mid = (maximumSweetnessThreshold + minimumSweetnessThreshold)/2;
-
-        while (mid < ) {
-
+        int mid;
+        int hi = maximumSweetnessThreshold;
+        int lo = minimumSweetnessThreshold;
+        while (lo < hi) {
+            mid = (lo + hi)/2;
+            System.out.println(mid + " " + maximumSweetnessThreshold + " " + minimumSweetnessThreshold + " can : " + canDivide(mid));
+            if (canDivide(mid) && !canDivide(mid -1)) {
+                return mid;
+            } else if (canDivide(mid)) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
+        return -1;
+    }
+
+    private boolean canDivide(int thresholdSweetness) {
+        int sweetnessSum = 0;
+        int cuts = 0;
+        for (Integer part : sweetnessBar) {
+            if (sweetnessSum + part >= thresholdSweetness) {
+                cuts++;
+                sweetnessSum = 0;
+            } else {
+                sweetnessSum += part;
+            }
+        }
+        return cuts == this.numberOfCuts;
+    }
+
+    public static void main(String[] args) {
+        DivideChocolate divideChocolate = new DivideChocolate(Arrays.asList(1,2,3,4,5,6,7,8,9), 5);
+        System.out.println(divideChocolate.maximumMinimumSweetnessSegmentAfterCutting());
+        divideChocolate = new DivideChocolate(Arrays.asList(5,6,7,8,9,1,2,3,4), 8);
+        System.out.println(divideChocolate.maximumMinimumSweetnessSegmentAfterCutting());
+        divideChocolate = new DivideChocolate(Arrays.asList(1,2,2,1,2,2,1,2,2), 2);
+        System.out.println(divideChocolate.maximumMinimumSweetnessSegmentAfterCutting());
     }
 }
