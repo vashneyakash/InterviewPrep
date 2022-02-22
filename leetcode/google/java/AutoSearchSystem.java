21b140748a97128b3c41c9fc11f4157fd6ebdb4c
vashneyakash <vashneyakash@gmail.com>
2022-02-22 11:18:49 +0530

    [brute-force-solution] Start AutoSearchSystem

---
+++ leetcode/google/java/AutoSearchSystem.java
@@ -0,0 +1,16 @@
+package google.java;
+
+public class AutoSearchSystem {
+    /*
+    * Optimal solution:
+    * trie:
+    * insert: O(length of text)
+    * search: O(length of text)
+    *
+    * map:
+    * insert: O(log(n))
+    * insert: O(log(n))
+    *
+    * Trie is more optimal as only length of text is used in complexity
+    * */
+}
