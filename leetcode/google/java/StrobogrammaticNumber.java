5cac53de54b6e651b97fac0d789360ad8b53eb80
vashneyakash <vashneyakash@gmail.com>
2022-02-27 11:49:39 +0530

    [optimal-solution] Think on StrobogrammaticNumber

---
+++ leetcode/google/java/StrobogrammaticNumber.java
@@ -0,0 +1,14 @@
+package google.java;
+
+public class StrobogrammaticNumber {
+    /*
+    * Optimal solution:
+    * Find number of strombatic numbers till x
+    * Use Digit DP
+    * count number of digits in x
+    * for digit number < digits in x:
+    *   calculate strombatic numbers
+    *
+    * specially calculate the strombatic numbers for digit number == digits in x
+    * */
+}
