af7e350fc032154b95331e66374a25884fc2c1ab
vashneyakash <vashneyakash@gmail.com>
2022-03-01 11:24:29 +0530

    [optimal-solution] StrobogrammaticNumber: Add video links for Digit DB that you watched

--- leetcode/google/java/StrobogrammaticNumber.java
+++ leetcode/google/java/StrobogrammaticNumber.java
@@ -10,5 +10,10 @@ public class StrobogrammaticNumber {
     *   calculate strombatic numbers
     *
     * specially calculate the strombatic numbers for digit number == digits in x
+    *
+    * Watched videos for digit DB
+    * https://www.youtube.com/watch?v=IjPGC8aN4vA
+    * https://www.youtube.com/watch?v=T5R6qZAp0u8
     * */
+
 }
