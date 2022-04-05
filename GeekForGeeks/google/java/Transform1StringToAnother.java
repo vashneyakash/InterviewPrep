a4208625a5847c00246b4d7f995b00687223b855
vashneyakash <vashneyakash@gmail.com>
2022-04-05 11:55:45 +0530

    [bruteforce-solution] Think on Transform1StringToAnother

---
+++ GeekForGeeks/google/java/Transform1StringToAnother.java
@@ -0,0 +1,13 @@
+package GeekForGeeks.google.java;
+
+public class Transform1StringToAnother {
+    /*
+    * Since only possible operation is to delete at char at index i and push it to front.
+    * We can check if both the strings has same freq of the characters. If yes then first can be transformed into second.
+    * To find min operations required ->
+    * 1. In n operations we can transform 1 string to another
+    * 2.
+    * abcdef -> fadecb
+    *
+    * */
+}
