0180a8f48e3658ab6765e28b49b1219f907da427
vashneyakash <vashneyakash@gmail.com>
2022-04-18 10:38:28 +0530

    [optional-solution] Code up BraceExpansionII

--- leetcode/google/java/BraceExpansionII.java
+++ leetcode/google/java/BraceExpansionII.java
@@ -1,7 +1,6 @@
 package google.java;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BraceExpansionII {
     /*
@@ -13,62 +12,170 @@ public class BraceExpansionII {
     *
     * */
 
-    public BraceExpansionII(String expression) {
+    public BraceExpansionII() {
     }
 
     List<String> evaluateExpr(String expression) {
-        List<String> subExpr = getAllSubExpr(expression);
+        List<Expression> subExpr = getAllSubExpr(expression);
+
+        List<SubExpressions> subSubExpressions = new ArrayList<>();
+        for(Expression expr: subExpr) {
+            List<String> subSubExpr = evaluateExpr(expr.expr());
+            subSubExpressions.add(new SubExpressions(subSubExpr, expr.operator()));
+        }
+
+        return subSubExpressions.stream().reduce((a,b) -> {
+            List<String> result  = new ArrayList<>();
+            if (Operator.cartesian.equals(a.operator())) {
+                for (String e1: a.subExpr()) {
+                    for (String e2: b.subExpr()) {
+                        result.add(e1 + e2);
+                    }
+                }
+            } else {
+                result.addAll(a.subExpr());
+                result.addAll(b.subExpr());
+            }
+            return new SubExpressions(result, b.operator());
+        }).orElseThrow(() -> new RuntimeException("Invalid expression"))
+                .subExpr();
     }
 
-    private List<String> getAllSubExpr(String expression) {
-        if (canSeparateByComma(expression)) {
+    private List<Expression> getAllSubExpr(String expression) {
+        System.out.println(expression);
+        if (isAtomicExpression(expression)) {
+            System.out.println("result : " + new Expression(expression, Operator.union));
+            return Collections.singletonList(new Expression(expression, Operator.union));
+        } if (canSeparateByComma(expression)) {
+            System.out.println("result Comma : " + separateByComma(expression));
             return separateByComma(expression);
         } else {
+            System.out.println("result Bracket : " + separateByBracket(expression));
             return separateByBracket(expression);
         }
+    }
+
+    boolean isAtomicExpression(String expression) {
+        return !(expression.contains(",") || expression.contains("{"));
     }
 
     private boolean canSeparateByComma(String expression) {
         int openBrackets = 0;
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '{') openBrackets++;
-            else if (expression.charAt(i) == '}') openBrackets++;
+            else if (expression.charAt(i) == '}') openBrackets--;
             else if (expression.charAt(i) == ',' && openBrackets == 0) return true;
         }
         return false;
     }
 
-    private List<String> separateByComma(String expression) {
+    private List<Expression> separateByComma(String expression) {
         int openBrackets = 0;
         int expressionStart = 0;
-        List<String> subExpressions = new ArrayList<>();
+        List<Expression> subExpressions = new ArrayList<>();
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '{') openBrackets++;
-            else if (expression.charAt(i) == '}') openBrackets++;
+            else if (expression.charAt(i) == '}') openBrackets--;
             else if (expression.charAt(i) == ',' && openBrackets == 0) {
-                subExpressions.add(expression.substring(expressionStart, i - 1));
+                subExpressions.add(new Expression(expression.substring(expressionStart, i), Operator.union));
                 expressionStart = i + 1;
             }
         }
-        subExpressions.add(expression.substring(expressionStart, expression.length() - 1));
+        if (expressionStart != expression.length()) {
+            subExpressions.add(new Expression(expression.substring(expressionStart, expression.length()), Operator.union));
+        }
         return subExpressions;
     }
-}
 
-    private List<String> separateByBracket(String expression) {
+    private List<Expression> separateByBracket(String expression) {
+        if (hasWrapperBraces(expression)) {
+            expression = expression.substring(1, expression.length() - 1);
+        }
         int openBrackets = 0;
         int expressionStart = 0;
-        List<String> subExpressions = new ArrayList<>();
+        List<Expression> subExpressions = new ArrayList<>();
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '{') {
                 openBrackets++;
-            } else if (expression.charAt(i) == '}') openBrackets++;
-            else if (expression.charAt(i) == ',' && openBrackets == 0) {
-                subExpressions.add(expression.substring(expressionStart, i - 1));
-                expressionStart = i + 1;
+                if (expressionStart != i && openBrackets == 1) {
+                    subExpressions.add(new Expression(expression.substring(expressionStart, i), Operator.cartesian));
+                    expressionStart = i;
+                }
+            } else if (expression.charAt(i) == '}') {
+                openBrackets--;
+                if (openBrackets == 0) {
+                    subExpressions.add(new Expression(expression.substring(expressionStart, i + 1), Operator.cartesian));
+                    expressionStart = i + 1;
+                }
             }
         }
-        subExpressions.add(expression.substring(expressionStart, expression.length() - 1));
+        if (expressionStart != expression.length()) {
+            subExpressions.add(new Expression(expression.substring(expressionStart, expression.length()), Operator.cartesian));
+        }
         return subExpressions;
+    }
+
+    private boolean hasWrapperBraces(String expression) {
+        Stack<Character> braces = new Stack<>();
+        for (int i = 1; i < expression.length() -1; i++) {
+            if (expression.charAt(i) == '{') braces.add('{');
+            else if (expression.charAt(i) == '}') {
+                if (!braces.isEmpty() && braces.peek() == '{') {
+                    braces.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return expression.charAt(0) == '{' && expression.charAt(expression.length() - 1) == '}' && braces.isEmpty();
+    }
+
+    public static class Expression {
+        private final String expr;
+        private final Operator operator;
+
+        public Expression(String expr, Operator operator) {
+            this.expr = expr;
+            this.operator = operator;
+        }
+
+        public String expr() {
+            return expr;
+        }
+
+        public Operator operator() {
+            return operator;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{ expr=%s, operator=%s }", this.expr, this.operator);
+        }
+    }
+
+    public static class SubExpressions {
+        private final List<String> expr;
+        private final Operator operator;
+
+        public SubExpressions(List<String> expr, Operator operator) {
+            this.expr = expr;
+            this.operator = operator;
+        }
+
+        public List<String> subExpr() {
+            return expr;
+        }
+
+        public Operator operator() {
+            return operator;
+        }
+    }
+
+    public enum Operator {
+        union, cartesian
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BraceExpansionII().evaluateExpr("{a,b}{c,{d,e}}"));
     }
 }
