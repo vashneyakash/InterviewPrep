1397aebfdf143d757a5437195da3394039c5c341
vashneyakash <vashneyakash@gmail.com>
2022-04-29 21:22:54 +0530

    Think on ParseLispExpression

---
+++ leetcode/google/java/ParseLispExpression.java
@@ -0,0 +1,11 @@
+package google.java;
+
+public class ParseLispExpression {
+    /*
+    * Approach :
+    * 1. Replace all the keywords with symbols (*,/,+,-)
+    * 2. Convert String to integer array
+    * 3. Infix to postfix conversion
+    * 4. Evaluate the expr
+    * */
+}
