5e44745dacab6c46c7bd139b64fc6f72a88ebee1
vashneyakash <vashneyakash@gmail.com>
2022-06-24 11:28:26 +0530

    Add MergeKSortedLists

---
+++ linkdein/MergeKSortedLists.java
@@ -0,0 +1,9 @@
+package linkdein;
+
+public class MergeKSortedLists {
+    /*
+    * To merge K lists
+    * 1. Merge 2 at a time Complexity O(K^2 * N)
+    * 2. Insert all the 1st elements in priorityQueue. then 1 by 1 O (log(k) * k * n)
+    * */
+}
