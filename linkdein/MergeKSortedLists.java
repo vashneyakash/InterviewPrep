6e6356e752e6a2c8bad0841daaf1ee499d8cd902
vashneyakash <vashneyakash@gmail.com>
2022-06-27 09:46:57 +0530

    Complete MergeKSortedLists

--- linkdein/MergeKSortedLists.java
+++ linkdein/MergeKSortedLists.java
@@ -17,15 +17,40 @@ public class MergeKSortedLists {
             return lists[0];
         } else {
             LinkedList<Integer> mergedList = new LinkedList<>();
+            for (LinkedList<Integer> list: lists) {
+                mergedList = mergedLists(list, mergedList);
+            }
+            return mergedList;
         }
     }
 
-    private static LinkedList<Integer> mergetLists(final LinkedList<Integer> first, final LinkedList<Integer> second) {
+    private static LinkedList<Integer> mergedLists(final LinkedList<Integer> first, final LinkedList<Integer> second) {
         LinkedList<Integer> mergedList = new LinkedList<>();
 
+        while (!first.isEmpty() && !second.isEmpty()) {
+            if (first.peekFirst() < second.peekFirst()) {
+                mergedList.add(first.pollFirst());
+            } else {
+                mergedList.add(second.pollFirst());
+            }
+        }
+
+        while (!first.isEmpty()) {
+            mergedList.add(first.pollFirst());
+        }
+
+        while (!second.isEmpty()) {
+            mergedList.add(second.pollFirst());
+        }
+
+        return mergedList;
     }
 
     public static void main(String[] args) {
-                    /// busy busy busy
+        LinkedList<Integer> l1 = new LinkedList<>(Arrays.asList(1, 4, 7, 10, 40));
+        LinkedList<Integer> l2 = new LinkedList<>(Arrays.asList(10, 14, 18, 30, 32));
+        LinkedList<Integer> l3 = new LinkedList<>(Arrays.asList(5, 6, 12, 15, 29));
+        LinkedList<Integer> l4 = new LinkedList<>(Arrays.asList(7, 13, 17, 45));
+        System.out.println(mergeLists(new LinkedList[] {l1, l2, l3, l4}));
     }
 }
