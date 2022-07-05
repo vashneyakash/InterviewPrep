520b9afedcfc5710b87b9f4bec0f93906dc6a464
vashneyakash <vashneyakash@gmail.com>
2022-07-05 22:52:48 +0530

    Think on SearchInRotatedSortedArray

---
+++ linkdein/SearchInRotatedSortedArray.java
@@ -0,0 +1,9 @@
+package linkdein;
+
+public class SearchInRotatedSortedArray {
+    /*
+    * 1. Find if the array is rotated or not by checking start and end elements
+    * 2. Find the inflection point by binary search
+    * 3. Find the ele using rotation formulae
+    * */
+}
