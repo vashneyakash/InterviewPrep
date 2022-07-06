8c3f5bcf0e34e2c3ecb8ed6f35f739bdc683a205
vashneyakash <vashneyakash@gmail.com>
2022-07-06 23:28:08 +0530

    Do SearchInRotatedSortedArray

--- linkdein/SearchInRotatedSortedArray.java
+++ linkdein/SearchInRotatedSortedArray.java
@@ -6,4 +6,30 @@ public class SearchInRotatedSortedArray {
     * 2. Find the inflection point by binary search
     * 3. Find the ele using rotation formulae
     * */
+
+    public static int offsetRotationIndex(int[] arr) {
+        if (arr[0] < arr[arr.length - 1]) return 0;
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi)/2;
+            if ((mid == 0 || arr[mid - 1] < arr[mid]) && (mid < (arr.length -1) || arr[mid] < arr[mid + 1])) {
+                return mid;
+            }
+
+            if ()
+        }
+    }
+
+    public static int findIndex(int[] arr) {
+        int n = arr.length;
+        int offset =
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
