33b6016c29ad1e532e8c9315da1891ecccb2b3b6
vashneyakash <vashneyakash@gmail.com>
2022-07-19 01:01:04 +0530

    Think on NQueens problem

---
+++ linkdein/NQueens.java
@@ -0,0 +1,9 @@
+package linkdein;
+
+public class NQueens {
+    /*
+    * for every position on chess board
+    *    iterate all other positions and check if the position empty
+    *
+    * */
+}
