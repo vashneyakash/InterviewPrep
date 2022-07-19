12fcb43e4609ed54c597a72a8d8491a8b65d1be4
vashneyakash <vashneyakash@gmail.com>
2022-07-19 23:54:54 +0530

    Watch video on N Queens

--- linkdein/NQueens.java
+++ linkdein/NQueens.java
@@ -4,6 +4,7 @@ public class NQueens {
     /*
     * for every position on chess board
     *    iterate all other positions and check if the position empty
-    *
+    * watched video on n queens - https://www.youtube.com/watch?v=gdz8H0_vKAo
+    * to be done by backtracking
     * */
 }
